41e658770c944b1caf550813d3b00b457d0434f4
jackliu612 <jackliu612@gmail.com>
2020-12-18 21:06:27 -0500

    Manual hat adding

---
+++ hat.js
@@ -0,0 +1,53 @@
+function SantaHat(x1, y1, x2, y2){
+
+    let width = x2 - x1;
+    let height = y2 - y1;
+    let x_offset = 0;
+    let y_offset = 0;
+
+    let img = loadImage('hat.png');
+
+
+    this.draw = function () {
+        image(img, x1-x_offset, y1-y_offset);
+    }
+
+    this.drawBox = function () {
+        noFill();
+        strokeWeight(10);
+        rect(x1, y1, width, height);
+    }
+
+    this.resize = function () {
+        img.resize(width*1.5, 0);
+        x_offset = width*.1
+        y_offset = height*.25;
+    }
+}
+
+function SantaBeard(x1, y1, x2, y2){
+
+    let width = x2 - x1;
+    let height = y2 - y1;
+    let x_offset = 0;
+    let y_offset = 0;
+
+    let img = loadImage('beard.png');
+
+
+    this.draw = function () {
+        image(img, x1-x_offset, y1-y_offset);
+    }
+
+    this.drawBox = function () {
+        noFill();
+        strokeWeight(10);
+        rect(x1, y1, width, height);
+    }
+
+    this.resize = function () {
+        img.resize(width, 0);
+        x_offset = 0
+        y_offset = -height*.65;
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,31 @@
+let WIDTH = 1000;
+let HEIGHT = 1000;
+
+let POINT_1_X = 320;
+let POINT_1_Y = 130;
+
+let POINT_2_X = 550;
+let POINT_2_Y = 440;
+
+let temp;
+let temp2;
+
+function preload() {
+    jack = loadImage('me.jpg');
+    temp = new SantaHat(POINT_1_X, POINT_1_Y, POINT_2_X, POINT_2_Y);
+    temp2 = new SantaBeard(POINT_1_X, POINT_1_Y, POINT_2_X, POINT_2_Y);
+}
+
+function setup() {
+    createCanvas(WIDTH, HEIGHT);
+    image(jack, 0, 0);
+    temp.resize();
+    temp2.resize();
+
+}
+
+function draw() {
+    temp.drawBox();
+    temp.draw();
+    temp2.draw();
+}
