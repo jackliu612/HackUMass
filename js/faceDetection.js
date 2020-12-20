e028c5bda6564e2aef021fc9dd28a9d345ffa5da
jackliu612 <jackliu612@gmail.com>
2020-12-19 19:41:13 -0500

    code refactoring

--- js/faceDetection.js
+++ js/faceDetection.js
@@ -67,30 +67,3 @@ imgElement.onload = function () {
 
 
 };
-
-/**
- * This is the onclick function to the submit button after the user chooses their accessories.
- * If a previous picture has been modified already, replace the current one with the new modified picture.
- * Creates a new p5 instance in either case.
- * @function submitChoices
- * @memberof faceDetection
- */
-function submitChoices() {
-    if (skch != null) {
-        skch.remove();
-    }
-    skch = new p5(sketch, 'output');
-}
-
-/**
- * This is the onclick function to the download button which allows the user
- * to download their edited photograph onto their computer.
- * @function download
- * @memberof faceDetection
- */
-function download() {
-    let download = document.getElementById("download");
-    let image = document.getElementById("holiday").toDataURL("image/png")
-        .replace("image/png", "image/octet-stream");
-    download.setAttribute("href", image);
-}
